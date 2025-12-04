6cdeb83460f4a06372722cac73379d2177b7eb66
Olek Stachura <133813772+OlekAS13@users.noreply.github.com>
2025-12-04 09:41:57 +0100

    Add files via upload

--- konwerter/konwerter.cpp
+++ konwerter/konwerter.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    ifstream menu("menu.txt");
+    int choice;
 
-    cout << menu;
+    cout << "\033[2J\033[H";
+    cout << "======================\n";
+    cout << "[KONWERTER JEDNOSTEK]\n\n";
+    cout << "1. - Konwertuj\n";
+    cout << "2. - Wyswietl historie\n";
+    cout << "3. - Ulubione\n\n\n";
+    cout << "======================";
+
+    cout << "\033[1A";
+    cout << "\r>> ";
+
+    cin >> choice;
+
+    // Konwertowanie
+    if (choice == 1) {
+        cout << "\033[2J\033[H";
+
+        cout << "======================\n";
+        cout << "[WYBOR JEDNOSTKI]\n\n";
+        cout << "1. - Metr\n";
+        cout << "2. - Cal\n";
+        cout << "3. - Mila\n\n\n";
+        cout << "======================";
+
+        cout << "\033[1A";
+        cout << "\r>> ";
+
+        cin >> choice;
+
+        // Konwertowanie z metrow
+        if (choice == 1) {
+            int value;
+
+            cout << "\033[2J\033[H";
+
+            cout << "======================\n";
+            cout << "[WARTOSC]\n\n";
+            cout << "======================";
+            cout << "\033[1A";
+            cout << "\r>> ";
+
+            cin >> value;
+        }
+    }
 }
