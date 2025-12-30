e93728d35882d1666936bb09d4158d14a326abd1
Ozvex <stas.maciorowski@gmail.com>
2025-12-30 16:58:08 +0100

    Add files via upload

--- konwerter/konwerter.cpp
+++ konwerter/konwerter.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-double meters, inches, miles;
+long double meters, inches, miles, kilometers;
 
 void drawMenu() {
     cout << "\033[2J\033[H";
@@ -26,7 +26,8 @@ void drawChoice() {
     cout << "[WYBOR JEDNOSTKI]\n\n";
     cout << "1. - Metr\n";
     cout << "2. - Cal\n";
-    cout << "3. - Mila\n\n\n";
+    cout << "3. - Mila\n";
+    cout << "4. - Kilometry\n\n\n";
     cout << "======================";
 
     cout << "\033[1A";
@@ -44,14 +45,15 @@ void drawGetValue(string which) {
     cout << "\r>> ";
 }
 
-void drawOutputValues() {
+void drawOutputValues(string which) {
     cout << "\033[2J\033[H";
 
     cout << "======================\n";
-    cout << "[KONWERSJA - metry]\n\n";
+    cout << "[KONWERSJA - " << which << "]\n\n";
     cout << "Metry: " << meters << " m\n";
     cout << "Cale: " << inches << " in\n";
-    cout << "Mile: " << miles << " mi\n\n";
+    cout << "Mile: " << miles << " mi\n";
+    cout << "Kilometry: " << kilometers << " km\n\n";
     cout << "======================";
 
     cout << "\033[1A";
@@ -67,12 +69,16 @@ int main() {
     // Konwertowanie
     if (choice == 1) {
         
+        fstream history;
+        history.open ("HistoriaKonwersji.txt",ios::out|ios::app);
+        
         drawChoice();
 
         cin >> choice;
 
         // Konwertowanie z metrow
         if (choice == 1) {
+            
             int value;
 
             drawGetValue("metry");
@@ -80,26 +86,90 @@ int main() {
             cin >> value;
 
             meters = value;
-            inches = meters * 0.0254;
-            miles = meters * 0.0006213712;
+            inches = meters * 39.3700787;
+            miles = meters * 0.0006213712; 
+            kilometers = meters * 1000;
             
-            drawOutputValues();
+            drawOutputValues("metry");
+            
+            history << "Konwertowanie z " << value << " metrów\n";
         }
 
+        // Konwertowanie z cali
         else if (choice == 2) {
+
             int value;
 
             drawGetValue("cale");
 
             cin >> value;
+            
+            inches = value;
+            meters = inches * 0.0254;
+            miles = inches * 0.0000158;  
+            kilometers = meters * 1000;
+            
+            drawOutputValues("cale");
+            
+            history << "Konwertowanie z " << value << " cali\n";
         }
 
+        // Konwertowanie z mil
         else if (choice == 3) {
+            
             int value;
 
             drawGetValue("mile");
 
             cin >> value;
+            
+            miles = value;
+            meters = miles * 1609.344;
+            inches = miles * 63360; 
+            kilometers = meters * 1000;
+            
+            drawOutputValues("mile");
+            
+            history << "Konwertowanie z " << value << " mil\n";
         }
+        
+        // Konwertowanie z kilometrow
+        else if (choice == 4) {
+            
+            int value;
+
+            drawGetValue("kilometry");
+
+            cin >> value;
+            
+            kilometers = value;
+            meters = kilometers * 0.001;
+            inches = meters * 39.3700787;
+            miles = meters * 0.0006213712; 
+            
+            drawOutputValues("kilometry");
+            
+            history << "Konwertowanie z " << value << " kilometrów\n";
+        }
+        history.close();
+    }
+    if (choice == 2) {
+
+        ifstream history ("HistoriaKonwersji.txt");
+        if (history.is_open())
+        {
+            cout << "\033[2J\033[H";
+
+            cout << "======================\n";
+            cout << "[HISTORIA KONWERSJI]\n\n";
+            char row[10000];
+            while (history.getline(row,10000))
+            {
+                cout << row << endl;
+            }
+            cout << "\n======================";
+        }
+        else cout << "Zadna konwersja nie zostala jeszcze wykonana";
+
     }
 }
